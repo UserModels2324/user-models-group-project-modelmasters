24f01b0f6652bf47833b8663913dff8cc8d3eb7d
YaraNimble <bikowski24@gmail.com>
2023-10-10 23:13:22 +0200

    Better wording + japanese facts

--- SS.py
+++ SS.py
@@ -3,7 +3,10 @@ from slimstampen.spacingmodel import SpacingModel, Fact, Response
 
 
 def add_facts(model):
-    facts = [["hello", "bonjour"], ["dog", "chien"], ["cat", "chat"], ["computer", "ordinateur"]]
+    #facts = [["hello", "bonjour"], ["dog", "chien"], ["cat", "chat"], ["computer", "ordinateur"]]
+    facts = [["hai", "yes"], ["lie", "no"], ["sumimasen", "excuse me"], ["arigatoo gozaimas", "thank you"],
+             ["tomodachi", "friend"], ["kazoku", "family"], ["kodomo", "children"], ["pan", "bread"],
+             ["asa-gohan", "breakfast"], ["hiru-gohan", "lunch"], ["yoru-gohan", "dinner"]]
     for index in range(len(facts)):
         curr_fact = facts[index]
         fact = Fact(index + 1, curr_fact[0], curr_fact[1])
@@ -12,25 +15,29 @@ def add_facts(model):
 
 def learn_fact(model):
     cont = "y"
-    start_time, end_time = 0, 0
     while cont.lower() == "y":
         next_fact, new = model.get_next_fact(current_time=time.perf_counter())
         start_time = time.perf_counter()
         if new:
             print(next_fact.question + " => " + next_fact.answer)
-            answer = input("Continue?\n")
+            answer = input("Continue?")
             corr = True
         else:
-            answer = input(next_fact.question + "\n")
+            answer = input(next_fact.question + " => ").lower()
             corr = (next_fact.answer == answer)
         response_time = time.perf_counter() - start_time
+        if not corr:
+            print(f"Sorry, that response it wrong. \nThe correct answer is: '{next_fact.answer}'")
+        elif not new:
+            print("That answer is correct!")
         resp = Response(fact=next_fact, start_time=start_time, rt=response_time, correct=corr)
         model.register_response(resp)
-        cont = input("Learn next fact? [y/n]\n")
+        if not new:
+            cont = input("Learn next fact? [y/n]\n")
 
 
 if __name__ == '__main__':
-    print(time.perf_counter())
+    print("Welkom to SlimStampen (Now with competitions!)")
     model = SpacingModel()
     add_facts(model)
     next_fact, new = model.get_next_fact(current_time=34000)
@@ -39,5 +46,4 @@ if __name__ == '__main__':
     except:
         model.export_data("error.csv")
 
-    print(time.perf_counter())
     model.export_data("data.csv")
